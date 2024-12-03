0aea02f1edc470b062fad9ebda5e9325feb4c360
mdowns202 <m.downs1994@gmail.com>
2024-12-02 21:54:14 -0500

    Made makecsv.py more modular Now takes sys.argv and uses agrv[1] as file Now uses argv[2:] as headers

--- src/day1/makecsv.py
+++ src/day1/makecsv.py
@@ -1,14 +1,16 @@
 import csv
+import sys
 from result import Err, Ok, Result
 
 
-def main():
-    """Format data as csv file. It was quicker to work with this simple file in Python."""
-    """* Was pretty easy to work with this type of file in Rust as well (Day 2, Part 1)*"""
+def main(args):
+    """Format data as csv file. It was quicker to work with this simple file in Python"""
+    """* Was pretty easy to work with this type of file in Rust as well *"""
 
     try:
-        with open("./location_ids.csv", "r+", newline="") as file:
-            match make_csv(file):
+        with open(args[1], "r+", newline="") as file:
+            headers = args[2:]
+            match make_csv(file, headers):
                 case Ok(_):
                     print(f"{file} successfully reformatted")
                 case Err(e):
@@ -17,21 +19,21 @@ def main():
         print(e)
 
 
-def make_csv(file) -> Result[None, Exception]:
-    header1 = "listA"
-    header2 = "listB"
+def make_csv(file, headers: list[str]) -> Result[None, Exception]:
     # Change line terminator; original data may have been in 'excel-tab' dialect
     buf = file.readlines()
     writer = csv.writer(file, lineterminator="\n")
     # Don't run if file is already formatted
     try:
-        if buf[0] != f"{header1},{header2}\n":
+        if buf[0] != ",".join(headers) + "\n":
             file.seek(0)
-            writer.writerow([header1, header2])
+            writer.writerow(headers)
             # Strip whitespace in line before writing row, if row not already formatted
             for line in buf:
                 newline = [
-                    word.strip() for word in line.split() if word and len(line.split()) > 1
+                    word.strip()
+                    for word in line.split()
+                    if word and len(line.split()) > 1
                 ]
                 og_line = (
                     [word.strip() for word in line.split(",")] if not newline else None
@@ -45,5 +47,6 @@ def make_csv(file) -> Result[None, Exception]:
 
     return Ok(None)
 
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
